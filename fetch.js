df2064aaad602141841f6c5bc51594cc8fb2a9ce
Mike van Rossum <mike@askmike.org>
2012-09-02 18:34:30 +0200

    refactored code to be more like functional code

--- fetch.js
+++ fetch.js
@@ -1,53 +1,54 @@
-var l = console.log
-,	sql = require('/config/CMDA-tweets/sql.js')
-,	twit = require('/config/CMDA-tweets/twitter.js')
-,	hashtags = [ 'cmda', 'iamhva', 'hvaiam' ]
+var l = function( m ) {
+    console.log( m );
+    return m;
+} 
+,   sql = require( '/config/CMDA-tweets/sql.js' )
+,   twit = require( '/config/CMDA-tweets/twitter.js' )
+,   hashtags = [ 'cmda', 'iamhva', 'hvaiam' ]
+,   _ = require( 'underscore' )
 
-// function that stuffs a tweet into my db
-,	insertTweet = function( tweet ) {
-	var ts = Math.round( Date.parse( tweet.created_at ) / 1000 );
-	
-	sql.query(
-		"INSERT IGNORE INTO `tweets` (`tweetID` ,`timestamp` ,`body` ,`source` ,`userID` ,`userName`, `fullName`) VALUES (?, ?, ?, ?, ?, ?, ?);"
-	,	[ tweet.id_str, ts, tweet.text, tweet.source, tweet.user.id, tweet.user.screen_name, tweet.user.name  ]
-	);
-	
-	l('inserted tweet from ' + tweet.user.name + ' at ' + new Date() + ' :' + tweet.text);
+
+// check if tweet contains one of the hashes
+,   checkTweet = function( tweet ) {
+    l('checking tweet : ' + text);
+
+    var text = tweet.text
+    ,   ret;
+
+    hashtags.forEach(function( hash ) {
+        if( text.indexOf( '#' + hash ) !== -1 ) ret = tweet;
+    });
+    return ret;
 }
 
-// connect to the twitter streaming api and watch for CMDA tweets
+// insert tweet in DB
+,   insertTweet = function( tweet ) {
+    if( !tweet ) return;
 
-//twit.stream('statuses/filter', { track: 'cmda,iamhva,hvaiam' }, function(stream) {
-twit.stream('statuses/filter', { track: hashtags.join(',') }, function(stream) {
-	
-	l('start watching the stream at ' + new Date());
-	
-	var str
-	,	len = hashtags.length
-	,	i;
-	
-	stream.on('data', function( tweet ) {
-		
-		str = tweet.text;
-		i = len;
-		
-		l('new tweet from the stream: ' + str);
-		
-		for( ;i--; ) {
-			if( str.indexOf( '#' + hashtags[i] ) !== -1 ) {
-				// `insert ignore` prevents tweets being added multiple times
-				insertTweet( tweet );
-			}
-		}	
-		
-	});
-	stream.on('end', function(response) {
-		l('disconnected');
-		l(response);
-	});
-	stream.on('destroy', function(response) {
-		l('con destroyed at ' + new Date());
-		l(response);
-		sql.end(); // Handle a 'silent' disconnection from Twitter, no end/error event fired
-	});
+    var ts = Math.round( Date.parse( tweet.created_at ) / 1000 );
+    sql.query(
+        "INSERT IGNORE INTO `tweets` (`tweetID` ,`timestamp` ,`body` ,`source` ,`userID` ,`userName`, `fullName`) VALUES (?, ?, ?, ?, ?, ?, ?);"
+    ,   [ tweet.id_str, ts, tweet.text, tweet.source, tweet.user.id, tweet.user.screen_name, tweet.user.name  ]
+    );
+    
+    l( 'inserted tweet from ' + tweet.user.name + ' at ' + new Date() + ' :' + tweet.text );
+}
+
+,   insert = _.compose( insertTweet, checkTweet )
+
+
+,   destroy = function() {
+    l( 'con destroyed at ' + new Date() );
+    sql.end();
+};
+
+// connect to the twitter streaming api and watch for CMDA tweets
+twit.stream( 'statuses/filter', { track: hashtags.join( ',' ) }, function( stream ) {
+    
+    l( 'start watching the stream at ' + new Date() );
+    
+    stream.on( 'data', insert );
+    stream.on( 'destroy', destroy );
 });
+
+
