aca8568375b82bbe08c0436ccd85445dedd80037
Mike van Rossum <mike@askmike.org>
2012-09-12 22:00:41 +0200

    now also poll the search API because of missing tweets

--- fetch.js
+++ fetch.js
@@ -1,37 +1,50 @@
 var l = function( m ) {
     console.log( m );
     return m;
-} 
+}
+,   hashtags = [ 'cmda', 'iamhva', 'hvaiam' ]
+
+// libs
 ,   sql = require( '/config/CMDA-tweets/sql.js' )
 ,   twit = require( '/config/CMDA-tweets/twitter.js' )
-,   hashtags = [ 'cmda', 'iamhva', 'hvaiam' ]
 ,   _ = require( 'underscore' )
-
 
 // check if tweet contains one of the hashes
 ,   checkTweet = function( tweet ) {
     var text = tweet.text
     ,   ret;
 
-    l('checking tweet : ' + text);
-
     hashtags.forEach(function( hash ) {
-        if( text.indexOf( '#' + hash ) !== -1 ) ret = tweet;
+        if( text.indexOf( '#' + hash ) !== -1 ) 
+            ret = tweet;
     });
     return ret;
 }
 
+,   logResult = function( err, r ) {
+    if( r.insertId !== 0 )
+        l( 'inserted id ' + r.insertId + ' at ' + new Date() );
+}
+
 // insert tweet in DB
 ,   insertTweet = function( tweet ) {
-    if( !tweet ) return;
+    if( !tweet ) 
+        return;
+
+    // search api provides user data different than streaming api
+    tweet.user = tweet.user || {
+        id: tweet.from_user_id
+    ,   screen_name: tweet.from_user
+    ,   name: tweet.from_user_name
+    }
 
     var ts = Math.round( Date.parse( tweet.created_at ) / 1000 );
+
     sql.query(
         "INSERT IGNORE INTO `tweets` (`tweetID` ,`timestamp` ,`body` ,`source` ,`userID` ,`userName`, `fullName`) VALUES (?, ?, ?, ?, ?, ?, ?);"
     ,   [ tweet.id_str, ts, tweet.text, tweet.source, tweet.user.id, tweet.user.screen_name, tweet.user.name  ]
+    ,   logResult
     );
-    
-    l( 'inserted tweet from ' + tweet.user.name + ' at ' + new Date() + ' :' + tweet.text );
 }
 
 ,   insert = _.compose( insertTweet, checkTweet )
@@ -40,19 +53,27 @@ var l = function( m ) {
 ,   destroy = function() {
     l( 'con destroyed at ' + new Date() );
     sql.end();
-};
+}
+
+// connect the twitter search api and fetch the 50 latest tweets
+,   search = function() {
+    twit.search('#cmda', { result_type: 'recent', rpp: 50 }, function(err, data) {
+      data.results.forEach( insert );
+    });
+}
+
+,   ping = function() {
+    l( 'still alive at ' + new Date() );
+}
+
+,   pump = _.compose( search, ping );
 
 // connect to the twitter streaming api and watch for CMDA tweets
 twit.stream( 'statuses/filter', { track: hashtags.join( ',' ) }, function( stream ) {
-    
     l( 'start watching the stream at ' + new Date() );
-    
     stream.on( 'data', insert );
     stream.on( 'destroy', destroy );
 });
 
-var ping = function() {
-    l( 'still alive at ' + new Date() );
-}
-
-setInterval( ping, 1000*60*5 );
+pump();
+setInterval( pump, 1000*60*5 ); // 5 min
